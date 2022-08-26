d12b78a9d2bde1031ee7d778fdcf3e9709ecfb62
Joe O'Connell <github@bravoindia.net>
2022-08-26 16:47:30 -0700

    First iteration on logger

--- log_test/log1.py
+++ log_test/log1.py
@@ -1,6 +1,16 @@
 import logging
+import logging.config
 
-def hello_world(name:str = 'Joe') -> str:
+logging.config.fileConfig('~/Documents/GitHub/Joes-Python-Ideas/logging.conf')
+
+logger = logging.getLogger('simpleExample')
+
+def hello_world(name:str = 'Joe', log_level:str = 'INFO') -> str:
     """Hello World function that prints 'Hello World, name', 
         after calling a few unnecessary functions to test the logs."""
-
+    logger.info('log1.hello_world() has begun')
+    output = 'Hello World, and Hello {name}'
+    logger.debug(output)
+    formatted_output = output.format(name=name)
+    logger.debug(formatted_output)
+    return formatted_output
