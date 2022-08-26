bfb11c14eeff68bedeed55a157780505d44f4cb9
Joe O'Connell <github@bravoindia.net>
2022-08-26 16:50:05 -0700

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@ from setuptools import find_packages, find_packages, setup
 
 setup(
     name='logging_learning',
-    version='0.0.1',
+    version='0.0.2',
     description='Package to test and learn about logging',
     url='git@github.com/ocjr/joes-python-ideas.git',
     author='Joe OConnell',
     author_email='github@bravoindia.net',
     license='unlicense',
     packages=find_packages(),
-    install_requires=[
-        'logging'
-    ],
     zip_safe=False
 )
