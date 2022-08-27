6d55f2f0f3d0f00f5f5de5fd1d494b15d936daa0
Joe O'Connell <github@bravoindia.net>
2022-08-26 22:01:43 -0700

    updated logging.conf

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='logging_learning',
-    version='0.0.36',
+    version='0.0.37',
     description='Package to test and learn about logging',
     url='git@github.com/ocjr/joes-python-ideas.git',
     author='Joe OConnell',
