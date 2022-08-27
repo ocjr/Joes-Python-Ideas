da96a09490188358c1a7c7b84a5c2f7332acc9de
Joe O'Connell <github@bravoindia.net>
2022-08-26 21:44:10 -0700

    More path issues

--- log_test/log1.py
+++ log_test/log1.py
@@ -3,15 +3,34 @@ import logging.config
 from pathlib import Path
 
 LOGGING_CONFIG = Path(__file__).parent /'config'/'logging.conf'
-logging.config.fileConfig(LOGGING_CONFIG)
+#logging.config.fileConfig(LOGGING_CONFIG)
 
-logger = logging.getLogger('simpleExample')
+#logger = logging.getLogger('simpleExample')
+
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create fileHandler and set level to DEBUG
+fh = logging.FileHandler(filename='test.log')
+fh.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(filename)s(%(lineno)s) - %(levelname)s - %(message)s')
+formatter.datefmt = '%m/%d/%Y %H:%M:%S %Z(%z)'
+
+# add formatter to fh
+fh.setFormatter(formatter)
+
+# add fh to logger
+logger.addHandler(fh)
 
 def hello_world(name:str = 'Joe', log_level:str = 'INFO') -> str:
     """Hello World function that prints 'Hello World, name', 
         after calling a few unnecessary functions to test the logs."""
     logger.info('log1.hello_world() has begun')
-    logger.info(Path(__file__).parent)
+    logger.info(LOGGING_CONFIG)
     output = 'Hello World, and Hello {name}'
     logger.debug('output object: ' + output)
     formatted_output = output.format(name=name)
