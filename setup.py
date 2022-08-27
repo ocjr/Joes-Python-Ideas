26c8bc7b543ae8f082d9e7d27cf78d2aa6f09077
Joe O'Connell <github@bravoindia.net>
2022-08-26 17:25:24 -0700

    Updated formatter

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='logging_learning',
-    version='0.0.11',
+    version='0.0.12',
     description='Package to test and learn about logging',
     url='git@github.com/ocjr/joes-python-ideas.git',
     author='Joe OConnell',
