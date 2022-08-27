0fd0a1ba1ac0da4683712484e65f152a5ea377de
Joe O'Connell <github@bravoindia.net>
2022-08-26 17:35:05 -0700

    Updated log messages

--- log_test/log1.py
+++ log_test/log1.py
@@ -13,7 +13,7 @@ def hello_world(name:str = 'Joe', log_level:str = 'INFO') -> str:
         after calling a few unnecessary functions to test the logs."""
     logger.info('log1.hello_world() has begun')
     output = 'Hello World, and Hello {name}'
-    logger.debug(output)
+    logger.debug('output object: ' + output)
     formatted_output = output.format(name=name)
-    logger.debug(formatted_output)
+    logger.debug('formatted_output object: ' + formatted_output)
     return formatted_output
